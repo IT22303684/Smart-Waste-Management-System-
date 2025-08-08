138baaefa5e8276961cd0bf0139b65382ac5a80f
Dasun <it22303684@my.sliit.lk>
2025-08-08 12:20:00 +0530

    feat: implemnted test server

---
+++ server.js
@@ -0,0 +1,46 @@
+import 'express-async-errors';
+
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+import express, { application } from 'express';
+const app = express();
+import morgan from 'morgan';
+import mongoose from 'mongoose';
+import cookieParser from 'cookie-parser';
+import cloudinary from 'cloudinary';
+
+
+
+
+
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './Client/dist', 'index.html'));
+});
+
+
+
+
+
+
+
+
+
+app.use('*', (req, res) => {
+    res.status(404).json({msg: 'route not found'});
+});
+
+
+app.use(errorHandelerMiddleware);
+
+
+try {
+    await mongoose.connect(process.env.MONGO_URL)
+    app.listen(PORT, () => {
+        console.log(`Server is running on ${PORT}`);
+      });
+}catch(error) {
+    console.log(error);
+}
+
